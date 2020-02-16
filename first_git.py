e2eec5281e00976858a11c572c3496791b2159c7
W Hotmail <winston_fong@hotmail.com>
2020-02-16 16:44:02 -0500

    Very 1st commit!

---
+++ first_git.py
@@ -0,0 +1,13 @@
+x = 0
+
+
+while True:
+    nm = input('Enter your name',)
+    conf_nm = input('is your name really 'nm, '?')
+    if conf_nm = 'y' or 'Y': break
+    or 'n' or 'N'
+
+if
+
+rep = input()
+print ('ur name iz, 'nm)
